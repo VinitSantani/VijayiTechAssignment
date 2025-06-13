d41b70f340d459317267b3338d15c1a81a559bf9
VinitSantani <78662493+VinitSantani@users.noreply.github.com>
2025-06-13 06:10:52 -0400

    Update feature_engineering.py

--- task1/feature_engineering.py
+++ task1/feature_engineering.py
@@ -1,1 +1,9 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from textblob import TextBlob
 
+def extract_features(df):
+    tfidf = TfidfVectorizer(max_features=500)
+    tfidf_matrix = tfidf.fit_transform(df['clean_text'])
+    df['ticket_length'] = df['ticket_text'].apply(len)
+    df['sentiment'] = df['ticket_text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    return tfidf_matrix, df[['ticket_length', 'sentiment']]
